52d17e8e481665aad441724356fb3d82b2d314ec
AIMageGuild <AIMageGuild@users.noreply.github.com>
2023-10-23 21:45:15 +0800

    upload

--- png2jpg.py
+++ png2jpg.py
@@ -7,7 +7,12 @@ def convert_png_to_jpg(root_folder):
         for filename in filenames:
             if filename.endswith(".png"):
                 png_path = os.path.join(dirpath, filename)
-                jpg_path = os.path.join(dirpath, filename[:-4] + ".jpg")
+                
+                # Clip the first 40 letters from the filename
+                new_filename = filename[40:]
+                
+                # Use the new_filename for the jpg_path
+                jpg_path = os.path.join(dirpath, new_filename[:-4] + ".jpg")
                 
                 with Image.open(png_path) as img:
                     img = img.convert("RGB")  # Convert the image to RGB mode
